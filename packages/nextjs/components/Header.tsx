5eafd165c53c0eb1bf261a17613b6be94b09e9bf
十五年 <zhongxiaomi06@gmail.com>
2026-08-15 18:22:19 +0800

    align frontend claims and remove template residue

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -20,7 +20,7 @@ export const menuLinks: HeaderMenuLink[] = [
     href: "/",
   },
   {
-    label: "Debug Contracts",
+    label: "Contract Console",
     href: "/debug",
     icon: <BugAntIcon className="h-4 w-4" />,
   },
